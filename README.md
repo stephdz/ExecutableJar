b3fa52490d00e4e9232c02cb3466c861060ebf8c
Stéphane DESHIERE <sdeshiere@yahoo.fr>
2013-01-02 18:29:04 +0100

    Premier commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ExecutableJar
+=============
+
+Exemple de projet Maven permettant de générer un zip contenant un jar prêt à être exécuté.
---
+++ src/main/java/fr/dz/exec/Main.java
@@ -0,0 +1,10 @@
+package fr.dz.exec;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+	private static final Logger LOGGER = Logger.getLogger(Main.class);
+	public static void main(String[] args) {
+		LOGGER.info("Hello world !");
+	}
+}
